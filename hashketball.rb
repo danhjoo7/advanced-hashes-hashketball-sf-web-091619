7015cb49036203e0a8df8448a8ed5c51aa61c822
Daniel Joo <danhjoo@gmail.com>
2019-09-14 19:32:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,10 +11,10 @@ def game_hash
                 {:brook_lopez => {number: 11, shoe: 17, points: 17, 
                   rebounds: 19, assists: 10, steals: 3, blocks: 1, 
                   slam_dunks: 15}},
-                {:mason_plumlee => number: 1, shoe: 19, points: 26, 
+                {:mason_plumlee => {number: 1, shoe: 19, points: 26, 
                   rebounds: 11, assists: 6, steals: 3, blocks: 8, 
                   slam_dunks: 5}},
-                {:jason_terry => number: 31, shoe: 15, points: 19, 
+                {:jason_terry => {number: 31, shoe: 15, points: 19, 
                   rebounds: 2, assists: 2, steals: 4, blocks: 11, 
                   slam_dunks: 1}}
                 ],
