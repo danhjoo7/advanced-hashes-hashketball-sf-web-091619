f922b1c1dc81aa438768a62a9aa63e6393fbdf05
Daniel Joo <danhjoo@gmail.com>
2019-09-14 19:15:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,11 @@
 def game_hash 
   stats = {
     :home => {:team_name =>  , :colors =>   , 
-              :players => [{:Alan_Anderson => {number: "0", shoe: "16", points:"
+              :players => [
+              {:alan_anderson => {number: "0", shoe: "16", points:"
               22", rebounds: "12", assists: "12", steals: "3", blocks: "1", 
-              slam_dunks: "1"}}],
+              slam_dunks: "1"}},
+              {:reggie_evans => {number: "30", shoe: "14", points: "12", rebounds: "12"}],
     :away => {:team_name =>  , :colors =>   , :players => }
   }
 end 
